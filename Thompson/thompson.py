3a385dc1915f8bc4aba083465b436b6229e13d20
Benedikt <benedikt_engel@gmx.de>
2024-10-07 03:25:00 +0200

    first test

--- Thompson/thompson.py
+++ Thompson/thompson.py
@@ -13,9 +13,17 @@ class Thompson_Bandit:
 
     def choose_arm(self):
         sampled_theta = [beta.rvs(a=self.p[i], b=self.q[i]) for i in range(self.n_arms)]
+        sort = sorted(sampled_theta, reverse=True)
+        second_largest = sort[1]
+        m = sampled_theta.index(second_largest)
         n = argmax(sampled_theta)
-        self.arm_counts[n] += 1
-        return n
+
+        if np.random.randint(1,3) == 2:
+            self.arm_counts[n] += 1
+            return n
+        else:
+            self.arm_counts[m] +=1
+            return m
 
     def simulate_env(self, arm, true_conversion_rates: np.ndarray):
         return 1 if np.random.rand() <= true_conversion_rates[arm] else 0
@@ -51,11 +59,16 @@ for i in range(n_arms):
 print(conversion_rate)
 ts = Thompson_Bandit(n_arms, alpha_params, beta_params)
 rewards = []
+optimal_reward = np.max(conversion_rate)
+cumulative_regret = np.zeros(n_rounds)
+
 for n in range(n_rounds):
     arm = ts.choose_arm()
     reward = ts.simulate_env(arm, conversion_rate)
     ts.update(arm, reward)
     rewards.append(reward)
+
+    cumulative_regret[n] = (optimal_reward - conversion_rate[arm]) + (cumulative_regret[n - 1] if n > 0 else 0)
 
 
 plt.figure(figsize=(16, 10))
@@ -64,4 +77,14 @@ plt.title("Häufigkeit der Arm-Auswahl")
 plt.xlabel("Arm (Anzeige)")
 plt.ylabel("Anzahl der Ziehungen")
 plt.savefig('häufigkeit_arme_thompson.pdf')
+plt.show()
+
+
+plt.figure(figsize=(16, 10))
+plt.plot(cumulative_regret, label='Kumulierter Regret', color='orange')
+plt.title("Kumulierter Regret über Runden")
+plt.xlabel("Runden")
+plt.ylabel("Kumulierter Regret")
+plt.legend()
+plt.savefig('kumulierter_regret_thompson.pdf')
 plt.show()
